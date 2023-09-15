23f49ee975f6a346cf379ec346559ec1aa604529
William Kaseu <wkaseu@seas.upenn.edu>
2023-09-15 17:15:49 -0500

    Connect api

--- frontend/people-app/src/api/connect.js
+++ frontend/people-app/src/api/connect.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3001"
+const BASE_URL = "https://social-media-for-pets.onrender.com"
 const apiConnection = axios.create({baseURL:BASE_URL})
-//
+
 export default apiConnection;
